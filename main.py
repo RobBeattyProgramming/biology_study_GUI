03eae4e5ea499e2308dc2badba1fc3b8acd044b1
robbeattyprogramming <robbeattyprogramming@gmail.com>
2025-03-05 09:04:53 -0500

    corrected get_values func to account for new dictionaries and now basic idea of program works

--- main.py
+++ main.py
@@ -2,93 +2,87 @@ import random
 from tkinter import *
 import csv
 
-questionDict = {}
+fullQuestionList = []
 rowOrder = 0
 with open('question_list.csv', mode='r') as file:
     reader = csv.reader(file)
     for line in reader:
-        questionDict[rowOrder] = line
-        rowOrder += 1
+        fullQuestionList.append(line)
 
 count = 0
-questionsChosen = []
-questionList = []
 randomNumberChosen = []
-orderedQuestionList = []
 
-while count < 5:
+orderedQuestionDict = {}
+randomizedQuestionDict = {}
+promptList = []
 
-    randomNumber = random.randint(1,6) #needs length of dict
+while count < 4:
+    randomNumber = random.randint(1, len(fullQuestionList) - 1) 
     if randomNumber in randomNumberChosen:
         continue
     randomNumberChosen.append(randomNumber)
+    currentQuestion = fullQuestionList[randomNumber]
 
-    orderedQuestionList.append(questionDict[randomNumber])
-    preshuffledList = questionDict[randomNumber]
-    questionPrompt = preshuffledList[0]
-    preshuffledList.remove(questionPrompt)
-    random.shuffle(preshuffledList)
-    preshuffledList.insert(0, questionPrompt)
+    orderedQuestionDict[currentQuestion[0]] = currentQuestion[1], currentQuestion[2], currentQuestion[3]
+    promptList.append(currentQuestion[0])
 
-    questionsChosen.append(preshuffledList)
-    questionList.append(questionsChosen[count])
+    questionPrompt = currentQuestion[0]
+    currentQuestion.remove(currentQuestion[0])
+    random.shuffle(currentQuestion)
+    randomizedQuestionDict[questionPrompt] = currentQuestion
 
-    questionCount = 1
-    questionCountList = []
-    while questionCount < 4:
-        randomCountNumber = random.randint(1, 3)
-        if randomCountNumber in questionCountList:
-            continue
-        questionCountList.append(randomCountNumber)
-
-        questionCount += 1
     count += 1
+
+#need to create list of prompts
+    #use lists as call keys before each label
+        #switch up function based on needs
 
 
 def get_values():
-    questionOneList = orderedQuestionList[0]
-    questionTwoList = orderedQuestionList[1]
-    questionThreeList = orderedQuestionList[2]
-    questionFourList = orderedQuestionList[3]
+    questionOneList = orderedQuestionDict[promptList[0]] 
+    questionTwoList = orderedQuestionDict[promptList[1]] 
+    questionThreeList = orderedQuestionDict[promptList[2]] 
+    questionFourList = orderedQuestionDict[promptList[3]] 
 
     if qOneAOne.get() == 1:
-        if questionOneAnswerOneValue == questionOneList[1]:
+        if questionOneAnswerOneValue == questionOneList[0]:
             questionOneSpaceTop.config(text="YES")
-    elif qOneATwo.get() == 1:
-        if questionOneAnswerTwoValue == questionOneList[1]:
+    if qOneATwo.get() == 1:
+       if questionOneAnswerTwoValue == questionOneList[0]:
             questionOneSpaceTop.config(text="YES")
-    elif qOneAThree.get() == 1:
-        if questionOneAnswerThreeValue == questionOneList[1]:
+    if qOneAThree.get() == 1:
+        if questionOneAnswerThreeValue == questionOneList[0]:
             questionOneSpaceTop.config(text="YES")
 
     if qTwoAOne.get() == 1:
-        if questionTwoAnswerOneValue == questionTwoList[1]:
+        if questionTwoAnswerOneValue == questionTwoList[0]:
             questionTwoSpaceTop.config(text="YES")
     elif qTwoATwo.get() == 1:
-        if questionTwoAnswerTwoValue == questionTwoList[1]:
+        if questionTwoAnswerTwoValue == questionTwoList[0]:
             questionTwoSpaceTop.config(text="YES")
     elif qTwoAThree.get() == 1:
-        if questionTwoAnswerThreeValue == questionTwoList[1]:
+        if questionTwoAnswerThreeValue == questionTwoList[0]:
             questionTwoSpaceTop.config(text="YES")
 
+
     if qThreeAOne.get() == 1:
-        if questionThreeAnswerOneValue == questionThreeList[1]:
+        if questionThreeAnswerOneValue == questionThreeList[0]:
             questionThreeSpaceTop.config(text="YES")
     elif qThreeATwo.get() == 1:
-        if questionThreeAnswerTwoValue == questionThreeList[1]:
+        if questionThreeAnswerTwoValue == questionThreeList[0]:
             questionThreeSpaceTop.config(text="YES")
     elif qThreeAThree.get() == 1:
-        if questionThreeAnswerThreeValue == questionThreeList[1]:
+        if questionThreeAnswerThreeValue == questionThreeList[0]:
             questionThreeSpaceTop.config(text="YES")
 
     if qFourAOne.get() == 1:
-        if questionFourAnswerOneValue == questionFourList[1]:
+        if questionFourAnswerOneValue == questionFourList[0]:
             questionFourSpaceTop.config(text="YES")
     elif qFourATwo.get() == 1:
-        if questionFourAnswerTwoValue == questionFourList[1]:
+        if questionFourAnswerTwoValue == questionFourList[0]:
             questionFourSpaceTop.config(text="YES")
     elif qFourAThree.get() == 1:
-        if questionFourAnswerThreeValue == questionFourList[1]:
+        if questionFourAnswerThreeValue == questionFourList[0]:
             questionFourSpaceTop.config(text="YES")
 
 root = Tk()
@@ -96,24 +90,23 @@ root = Tk()
 root.title("Biology Beginner Questions")
 root.geometry('400x800')
 
-
-questionOneInfo = questionList[0]
-questionOneLabel = Label(root, text = questionOneInfo[0])
+questionOneInfo = randomizedQuestionDict[promptList[0]]
+questionOneLabel = Label(root, text = promptList[0])
 questionOneLabel.grid(column = 0, row = 0)
 
 qOneAOne = IntVar()
 qOneATwo = IntVar()
 qOneAThree = IntVar()
 
-questionOneAnswerOne = Checkbutton(root, text = questionOneInfo[1], variable=qOneAOne, onvalue=1, offvalue=0) 
+questionOneAnswerOne = Checkbutton(root, text = questionOneInfo[0], variable=qOneAOne, onvalue=1, offvalue=0) 
 questionOneAnswerOne.grid(column = 0, row = 1)
-questionOneAnswerOneValue = questionOneInfo[1]
-questionOneAnswerTwo = Checkbutton(root, text = questionOneInfo[2], variable=qOneATwo)
+questionOneAnswerOneValue = questionOneInfo[0] 
+questionOneAnswerTwo = Checkbutton(root, text = questionOneInfo[1], variable=qOneATwo, onvalue=1, offvalue=0)
 questionOneAnswerTwo.grid(column = 0, row = 2)
-questionOneAnswerTwoValue = questionOneInfo[2]
-questionOneAnswerThree = Checkbutton(root, text = questionOneInfo[3], variable=qOneAThree)
+questionOneAnswerTwoValue = questionOneInfo[1]
+questionOneAnswerThree = Checkbutton(root, text = questionOneInfo[2], variable=qOneAThree, onvalue=1, offvalue=0)
 questionOneAnswerThree.grid(column = 0, row = 3)
-questionOneAnswerThreeValue = questionOneInfo[3]
+questionOneAnswerThreeValue = questionOneInfo[2]
 
 questionOneSpaceTop = Label(root, text = "")
 questionOneSpaceTop.grid(column=0,row=4)
@@ -121,23 +114,23 @@ questionOneSpaceBottom = Label(root, text = "")
 questionOneSpaceBottom.grid(column=0,row=5)
 
 
-questionTwoInfo = questionList[1]
-questionTwoLabel = Label(root, text = questionTwoInfo[0])
+questionTwoInfo = randomizedQuestionDict[promptList[1]]
+questionTwoLabel = Label(root, text = promptList[1])
 questionTwoLabel.grid(column = 0, row = 6)
 
 qTwoAOne = IntVar()
 qTwoATwo = IntVar()
 qTwoAThree = IntVar()
 
-questionTwoAnswerOne = Checkbutton(root, text = questionTwoInfo[1], variable=qTwoAOne)
+questionTwoAnswerOne = Checkbutton(root, text = questionTwoInfo[0], variable=qTwoAOne)
 questionTwoAnswerOne.grid(column = 0, row = 7)
-questionTwoAnswerOneValue = questionTwoInfo[1]
-questionTwoAnswerTwo = Checkbutton(root, text = questionTwoInfo[2], variable=qTwoATwo)
+questionTwoAnswerOneValue = questionTwoInfo[0]
+questionTwoAnswerTwo = Checkbutton(root, text = questionTwoInfo[1], variable=qTwoATwo)
 questionTwoAnswerTwo.grid(column = 0, row = 8)
-questionTwoAnswerTwoValue = questionTwoInfo[2]
-questionTwoAnswerThree = Checkbutton(root, text = questionTwoInfo[3], variable=qTwoAThree)
+questionTwoAnswerTwoValue = questionTwoInfo[1]
+questionTwoAnswerThree = Checkbutton(root, text = questionTwoInfo[2], variable=qTwoAThree)
 questionTwoAnswerThree.grid(column = 0, row = 9)
-questionTwoAnswerThreeValue = questionTwoInfo[3]
+questionTwoAnswerThreeValue = questionTwoInfo[2]
 
 questionTwoSpaceTop = Label(root, text = "")
 questionTwoSpaceTop.grid(column=0,row=10)
@@ -145,23 +138,23 @@ questionTwoSpaceBottom = Label(root, text = "")
 questionTwoSpaceBottom.grid(column=0,row=11)
 
 
-questionThreeInfo = questionList[2]
-questionThreeLabel = Label(root, text = questionThreeInfo[0])
+questionThreeInfo = randomizedQuestionDict[promptList[2]]
+questionThreeLabel = Label(root, text = promptList[2])
 questionThreeLabel.grid(column=0,row=12)
 
 qThreeAOne = IntVar()
 qThreeATwo = IntVar()
 qThreeAThree = IntVar()
 
-questionThreeAnswerOne = Checkbutton(root, text = questionThreeInfo[1], variable=qThreeAOne)
+questionThreeAnswerOne = Checkbutton(root, text = questionThreeInfo[0], variable=qThreeAOne)
 questionThreeAnswerOne.grid(column = 0, row = 13)
-questionThreeAnswerOneValue = questionThreeInfo[1]
-questionThreeAnswerTwo = Checkbutton(root, text = questionThreeInfo[2], variable=qThreeATwo)
+questionThreeAnswerOneValue = questionThreeInfo[0]
+questionThreeAnswerTwo = Checkbutton(root, text = questionThreeInfo[1], variable=qThreeATwo)
 questionThreeAnswerTwo.grid(column = 0, row = 14)
-questionThreeAnswerTwoValue = questionThreeInfo[2]
-questionThreeAnswerThree = Checkbutton(root, text = questionThreeInfo[3], variable=qThreeAThree)
+questionThreeAnswerTwoValue = questionThreeInfo[1]
+questionThreeAnswerThree = Checkbutton(root, text = questionThreeInfo[2], variable=qThreeAThree)
 questionThreeAnswerThree.grid(column = 0, row = 15)
-questionThreeAnswerThreeValue = questionThreeInfo[3]
+questionThreeAnswerThreeValue = questionThreeInfo[2]
 
 questionThreeSpaceTop = Label(root, text = "")
 questionThreeSpaceTop.grid(column=0,row=16)
@@ -169,23 +162,23 @@ questionThreeSpaceBottom = Label(root, text = "")
 questionThreeSpaceBottom.grid(column=0,row=17)
 
 
-questionFourInfo = questionList[3]
-questionFourLabel = Label(root, text = questionFourInfo[0])
+questionFourInfo = randomizedQuestionDict[promptList[3]]
+questionFourLabel = Label(root, text = promptList[3])
 questionFourLabel.grid(column=0,row=18)
 
 qFourAOne = IntVar()
 qFourATwo = IntVar()
 qFourAThree = IntVar()
 
-questionFourAnswerOne = Checkbutton(root, text = questionFourInfo[1], variable=qFourAOne)
+questionFourAnswerOne = Checkbutton(root, text = questionFourInfo[0], variable=qFourAOne)
 questionFourAnswerOne.grid(column = 0, row = 19)
-questionFourAnswerOneValue = questionFourInfo[1]
-questionFourAnswerTwo = Checkbutton(root, text = questionFourInfo[2], variable=qFourATwo)
+questionFourAnswerOneValue = questionFourInfo[0]
+questionFourAnswerTwo = Checkbutton(root, text = questionFourInfo[1], variable=qFourATwo)
 questionFourAnswerTwo.grid(column = 0, row = 20)
-questionFourAnswerTwoValue = questionFourInfo[2]
-questionFourAnswerThree = Checkbutton(root, text = questionFourInfo[3], variable=qFourAThree)
+questionFourAnswerTwoValue = questionFourInfo[1]
+questionFourAnswerThree = Checkbutton(root, text = questionFourInfo[2], variable=qFourAThree)
 questionFourAnswerThree.grid(column = 0, row = 21)
-questionFourAnswerThreeValue = questionFourInfo[3]
+questionFourAnswerThreeValue = questionFourInfo[2]
 
 questionFourSpaceTop = Label(root, text = "")
 questionFourSpaceTop.grid(column=0,row=22)
@@ -195,9 +188,3 @@ questionFourSpaceBottom.grid(column=0,row=23)
 submitButton = Button(root, text = "Submit", command=get_values)
 submitButton.grid(column=0,row=25)
 
-
-
-
-
-
-# questions are answered and list from dictionary is used to determine answers
